04fde7c7e38e303235458f0b535aee2dd16d4c62
James Ault <james.ault@johnlewis.co.uk>
2019-03-11 15:43:43 +0000

    App file updated to ustilise datamapper

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require 'sinatra/base'
 
 class Makersbnb < Sinatra::Base
 
+  if ENV['RACK_ENV'] == 'test'
+    DataMapper.setup(:default, 'postgres://postgres@127.0.0.1:5432/chitter_test')
+    DataMapper.finalize
+    DataMapper.auto_upgrade!
+  else
+    DataMapper.setup(:default, 'postgres://postgres@127.0.0.1:5432/chitter_prod')
+    DataMapper.finalize
+    DataMapper.auto_upgrade!
+  end
+
 get '/' do
-  "Welcome to Makersbnb!"
+  'Welcome to Makers BnB'
+end
+
+get '/signup' do
+  # erb :FIND OUT NAME FROM OTHER TEAM
 end
 
 run! if app_file == $0
