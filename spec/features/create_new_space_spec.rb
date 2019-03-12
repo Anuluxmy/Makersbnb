0f2ff2a67b39e29d2468a737c78a567cac5fe7c6
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-12 14:24:17 +0000

    create space form and feature test added

--- spec/features/create_new_space_spec.rb
+++ spec/features/create_new_space_spec.rb
@@ -1,9 +1,27 @@
 feature 'Creating new space' do
-  scenario 'A signed in user can create a new space' do
+
+  scenario 'A signed in user can access the create space page' do
     sign_up
     sign_in
     click_button('List of Spaces')
     click_button('Create Space')
     expect(page).to have_content 'Please fill in this form'
   end
+
+  scenario 'A signed in user can create a new space' do
+    sign_up
+    sign_in
+    click_button('List of Spaces')
+    click_button('Create Space')
+    fill_in :space_name, with: 'test space'
+    fill_in :description, with: 'test description'
+    fill_in :price_per_night, with: '700'
+    fill_in :from_day, with: '1'
+    page.select('January', :from => 'from_month')
+    fill_in :to_day, with: '10'
+    page.select('January', :from => 'to_month')
+    click_button('Submit')
+    expect(page).to have_content 'Here are the available spaces'
+  end
+
 end
