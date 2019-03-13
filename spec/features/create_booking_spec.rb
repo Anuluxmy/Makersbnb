6e882e3a570434842c3eef0699f357174b6ae9d8
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-13 15:45:54 +0000

    passing tests for booking request and status

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -1,5 +1,5 @@
 feature 'creates a booking' do
-  scenario 'A signed in user can see a list of booking' do
+  scenario 'A signed in user can see a list of spaces' do
     sign_up
     sign_in
     create_space
@@ -9,8 +9,6 @@ feature 'creates a booking' do
     guest_sign_in
     click_button('List of Spaces')
     expect(page).to have_content('test space')
-    # click_button('Book test space')
-    #update and complete test
   end
 
   scenario 'Guest can make a booking' do
@@ -26,7 +24,8 @@ feature 'creates a booking' do
     expect(page).to have_content('Select preferred dates')
     fill_in :day_guest, with: '2'
     page.select('January', :from => 'month_guest')
-
+    click_button('Submit')
+    expect(page).to have_content('new')
   end
 
 end
