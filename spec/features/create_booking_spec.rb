54c1021f762eaffe6d725bc77c8fe007f854002a
anu <anu_iv2002@hotmail.com>
2019-03-13 11:22:34 +0000

    added feature test for booking

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -1,5 +1,5 @@
 feature 'creates a booking' do
-  scenario 'A signed in user can create a booking' do
+  scenario 'A signed in user can see a list of booking' do
     sign_up
     sign_in
     create_space
@@ -12,4 +12,13 @@ feature 'creates a booking' do
     # click_button('Book test space')
     #update and complete test
   end
+
+  # scenario 'Guest can make a booking' do
+  #   guest_sign_up
+  #   guest_sign_in
+  #   click_button('List of Spaces')
+  #   # click_button('Book')
+  #   expect(page).to have_content('Enter the dates')
+  # end
+
 end
