dbd6cc496a1dd8f2eb6d95873dc690042684915d
anu <anu_iv2002@hotmail.com>
2019-03-13 11:06:11 +0000

    adding guest test

--- spec/features/create_booking_spec.rb
+++ spec/features/create_booking_spec.rb
@@ -3,6 +3,12 @@ feature 'creates a booking' do
     sign_up
     sign_in
     create_space
+    visit('/')
+    click_button 'Log Out'
+    guest_sign_up
+    guest_sign_in
+    click_button('List of Spaces')
+    expect(page).to have_content('test space')
     # click_button('Book test space')
     #update and complete test
   end
